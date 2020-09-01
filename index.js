1b47ec380804ecf7eb7d2e9d88a7dc21b3e7845c
Chris <cciric44@gmail.com>
2020-08-31 22:06:06 -0400

    finally able to parse json array in cartarray.json

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const server = express();
 const fs = require('fs');
+//const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const loadJsonFile = require('load-json-file');
+
 server.use(cors());
-server.use(bodyParser.json())
+server.use(bodyParser.json());
+//server.use(cookieParser);
+server.use(session({secret:'Its a secret', resave:'false', saveUninitialized:'true'}));
 
+var sess;
 const port = 3000;
+const cartArrayStore = [];
+let rawData = '';
 
-server.get('/hello', (req, res) => res.send('Hello!'));
+
+server.get('/hello', (req, res) => {
+    sess = req.session;
+  loadJsonFile('./cartarray.json').then(json => {
+   let array = json;
+    res.send(array[0]);
+});
+
+    
+});
 
 server.post('/additem', (req, res) => {
     const item = req.body;
@@ -21,6 +39,7 @@ server.post('/additem', (req, res) => {
       });
 
       res.status(200);
+      
       res.json(req.body);
 })
 
