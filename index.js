25c032649df656decf78d430c232b7fb127a93d4
Chris <cciric44@gmail.com>
2020-09-02 22:56:19 -0400

    the sess.caritems are now saved to the file

--- index.js
+++ index.js
@@ -22,18 +22,41 @@ server.get('/hello', (req, res) => {
     sess = req.session;
   loadJsonFile('./cartarray.json').then(json => {
    let array = json;
-    res.send(array[0]);
+    //res.sendStatus(array.length);
 });
-
     
 });
 
+server.get('/cartitemtotal', (req, res) => {
+    sess = req.session;
+    sess.totalCart;
+    let array = null;
+    loadJsonFile('./cartarray.json').then(json => {
+        array = json;
+        sess.totalCart = array.length;
+        let num = sess.totalCart;
+        //console.log(String(sess.totalCart));
+        //console.log(JSON.parse({"total":sess.totalCart}));
+        console.log(JSON.stringify({"total":sess.totalCart}));
+        //  res.send(JSON.stringify({"total":sess.totalCart}));
+         res.send(num.toString());
+        
+     }).catch((err) => console.log(err));
+    //  console.log(sess.totalCart);
+    //  res.send(sess.totalCart);
+     
+})
+
 server.post('/additem', (req, res) => {
+    sess = req.session;
+    sess.cartContents;
+    console.log(sess);
     const item = req.body;
 
     let cartText = JSON.stringify(item);
-
-   fs.writeFile('cartarray.json', cartText, {'flag':'w'}, (err) => {
+    sess.cartContents = JSON.stringify(item);
+    console.log(sess.cartContents);
+   fs.writeFile('cartarray.json', sess.cartContents, {'flag':'w'}, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
       });
@@ -41,6 +64,7 @@ server.post('/additem', (req, res) => {
       res.status(200);
       
       res.json(req.body);
+      
 })
 
 server.listen(port, () => console.log(`app listening at port number ${port}`)); 
