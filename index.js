65b04847eeaee0ffd8873bd177c9939a85816906
Chris <cciric44@gmail.com>
2020-09-07 23:18:05 -0400

    added delete request to delete all contents

--- index.js
+++ index.js
@@ -80,6 +80,21 @@ server.post('/additem', (req, res) => {
       
 });
 
+server.delete('/clearAllCart', (req, res) => {
+    let emptyArray = [];
+
+    fs.writeFile('cartarray.json', JSON.stringify(emptyArray), {'flag':'w'}, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+
+      req.session.destroy();
+      req.session.cartContents.destroy();
+
+      res.status(200);
+      res.send({ result: 'OK', message: 'Session destroyed' });
+});
+
 // server.post('/additem', (req, res) => {
 //     sess = req.session;
 //     sess.cartContents;
